fb8b527fbe5c39a4fbcd7434510bf418a57b764f
sekul hassan <sekulhassan3031@gmail.com>
2023-12-03 12:04:07 +0600

    Error done

--- src/CountComponent/Typing.jsx
+++ src/CountComponent/Typing.jsx
@@ -3,7 +3,7 @@ import '../CountComponent/App.css';
 const Typing = () => {
   return (
    <div>
-     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias aperiam assumenda culpa delectus illum natus pariatur quos. Accusantium aut nesciunt officia officiis quam quidem, reiciendis sapiente voluptas voluptate voluptatem!
+       assumenda culpa delectus illum natus pariatur quos. Accusantium aut nesciunt officia officiis quam quidem, reiciendis sapiente voluptas voluptate voluptatem!
    </div>
   );
 };
